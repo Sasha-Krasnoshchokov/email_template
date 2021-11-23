03d18f4bc3d76807106d0ebfe2732f105998a8ba
Krasnoshchokokv Oleksandr <krasnoshchokovoleksandr.gmail.com>
2021-11-23 19:59:11 +0200

    First page

--- src/components/Main/Templates/Templates.js
+++ src/components/Main/Templates/Templates.js
@@ -1,10 +1,11 @@
-
+import { EmailTemplate } from './EmailTemplate/EmailTemplate.js';
 import './Templates.scss';
 
 export function Templates() {
   return (
     <>
       <div className="templates">
+        <EmailTemplate />
       </div>
     </>
   );
